8a99b0fb866cf5a2fbb5d37318e1b752ef0a26af
Stas-Buzunko <buzunkostas@gmail.com>
2017-03-19 18:19:41 +0300

    add course to disciplines

--- src/routes/Disciplines/components/MainView.js
+++ src/routes/Disciplines/components/MainView.js
@@ -1,7 +1,71 @@
-import React from 'react'
+import React, { Component } from 'react'
+import firebase from 'firebase'
 
-const MainView = ({ params }) => (
-  <div>Here will be courses for {params.discipline}</div>
-)
+class MainView extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      courses: [],
+      coursesFetched: false
+    }
+  }
+
+  componentWillMount () {
+    const { params } = this.props
+    this.fetchCourses(params.discipline)
+  }
+
+  componentWillReceiveProps (nextProps) {
+    if (this.props.params.discipline !== nextProps.params.discipline) {
+      this.fetchCourses(nextProps.params.discipline)
+    }
+  }
+
+  fetchCourses (discipline) {
+    firebase.database().ref('courses')
+    .orderByChild('discipline')
+    .equalTo(discipline)
+    .once('value')
+    .then(snapshot => {
+      const object = snapshot.val()
+
+      if (object !== null) {
+        const courses = Object.keys(object).map(id => ({ ...object[id], id }))
+        this.setState({ courses, coursesFetched: true })
+      } else {
+        this.setState({ coursesFetched: true })
+      }
+    })
+  }
+
+  rederCourses () {
+    const { courses } = this.state
+
+    return courses.map((course, i) => (
+      <div key={i}>
+        Description: {course.description}
+      </div>
+    ))
+  }
+
+  render () {
+    const { coursesFetched, courses } = this.state
+    const { params } = this.props
+
+    if (!coursesFetched) {
+      return (<div>Loading...</div>)
+    }
+
+    return (
+      <div>
+        <h3>Total: {courses.length} course for {params.discipline} found</h3>
+        <div>
+          {this.rederCourses()}
+        </div>
+      </div>
+    )
+  }
+}
 
 export default MainView
