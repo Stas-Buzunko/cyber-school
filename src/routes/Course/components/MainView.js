7c7c6c5c6e5878a7772b446a8bcf6f39c10bd39d
msZlets <anastasiyayelets@gmail.com>
2017-04-10 23:38:39 +0300

    problemCourseMainView

--- src/routes/Course/components/MainView.js
+++ src/routes/Course/components/MainView.js
@@ -13,9 +13,11 @@ class MainView extends Component {
       course: {},
       courseLoaded: false,
       lessons: [],
-      lessonsLoaded: false
-
+      lessonsLoaded: false,
+      sections: [],
+      courseLessons: []
     }
+    this.renderSectionsList = this.renderSectionsList.bind(this)
   }
 
   componentWillMount () {
@@ -37,14 +39,21 @@ class MainView extends Component {
       const object = snapshot.val()
       if (object !== null) {
         const course = { ...snapshot.val(), id }
-        const promises = course.lessonsIds.map(id =>
-          firebase.database().ref('lessons/' + id)
-          .once('value')
-          .then(snapshot2 => ({ ...snapshot2.val(), id }))
-        )
-        Promise.all(promises).then(lessons => {
+        const { sections } = course
+        const newSections = sections.map(section => {
+          const promises = section.lessonsIds.map(id =>
+            firebase.database().ref('lessons/' + id)
+            .once('value')
+            .then(snapshot2 => ({ ...snapshot2.val(), id }))
+          )
+          return Promise.all(promises).then(lessons => {
+            section = { ...section, lessons }
+            return (section)
+          })
+        })
+        Promise.all(newSections).then(result => {
           this.setState({
-            lessons,
+            sections: result,
             course,
             comments: course.comments,
             courseLoaded: true,
@@ -57,36 +66,59 @@ class MainView extends Component {
     })
   }
 
-  renderLessonsList () {
-    const { lessons } = this.state
+  renderLessonsList (lessons = []) {
     return (
-      <div className='col-xs-12 col-md-12'>
-        <div className='col-xs-12 col-md-8'>
-          <table className='table'>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Length</th>
-              </tr>
-            </thead>
-            <tbody>
-              {lessons.map((item, i) =>
-                <tr key={i}>
-                  <td>
-                    {item.isFree &&
-                      <Link to={{ pathname: `/lesson/${item.id}` }}>{item.name}</Link> }
+
+    <tbody>
+          {lessons.map((item, i) =>
+<tr>
+                <td>
+                  {item.isFree &&
+                    <Link to={{ pathname: `/lesson/${item.id}` }}>{item.name}</Link> }
                     {!item.isFree &&
                       <div>{item.name}</div> }
-                  </td>
-                  <td> {item.length} </td>
-                </tr>
+                    </td>
+                    <td> {item.length} </td>
+
+</tr>
               )}
-            </tbody>
-          </table>
-        </div>
-      </div>
-    )
-  }
+  </tbody>
+
+        )
+      }
+
+      renderSectionsList () {
+        const { sections = [] } = this.state
+        return (
+          <div className='col-xs-12 col-md-12'>
+            <div className='col-xs-12 col-md-8'>
+              <table className='table'>
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Length</th>
+                  </tr>
+                </thead>
+                {sections.map((item, i) =>
+                  <table className='table'>
+                    <tbody key={i}>
+                      <tr >
+                        <td>
+                          <div>{item.name}</div>
+                        </td>
+                        <td>  </td>
+                      </tr>
+                    </tbody>
+                  <table>
+                      {this.renderLessonsList(item.lessons)}
+</table>
+                  </table>
+                )}
+              </table>
+            </div>
+          </div>
+        )
+      }
 
   renderBuyButton () {
     const { course, courseLoaded } = this.state
@@ -154,7 +186,7 @@ class MainView extends Component {
         <div className='col-xs-6 col-md-10' style={{ padding: '15px' }}>
           <label className='control-label col-xs-8' style={{ padding: '15px' }}>Lessons: </label>
           <ul className='list-unstyled'>
-            {this.renderLessonsList()}
+            {this.renderSectionsList()}
           </ul>
         </div>
       </div>
